217d972abeaaf4f8c018035b26c465f707b84b23
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2024-08-05 08:43:38 -0300

    cambio el puerto donde escucha la db en respository/commands

--- app-monitor/src/infrastructure/repository/commands/index.ts
+++ app-monitor/src/infrastructure/repository/commands/index.ts
@@ -5,7 +5,7 @@ import { http } from "../../http";
 
 export const commandRepository = {
     getCommandList: async ():Promise<CommandItem[]> => {
-        const commandList = await http.get<CommandDTO[]>('http://localhost:3000/commandList');
+        const commandList = await http.get<CommandDTO[]>('http://localhost:4000/commandList');
         console.log(commandList)
         return commandList.map((commandItem:CommandDTO):CommandItem => ({
             id: commandItem.id,
